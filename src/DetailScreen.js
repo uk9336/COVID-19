68ea91c8c5e9be94cbd7327a66b12cdbbf16fc6f
이지원 <uk9336@naver.com>
2022-04-20 22:59:33 +0900

    * 현재 날짜 기준 공공데이터 파싱 	> 현재 날짜 작업 진행 	> api 함수 params 연결 	> 좌,우 버튼 추가 	> 클릭 이벤트 등록
    # 코드정리 및 클래스 사용 여부 확인 필요

--- src/DetailScreen.js
+++ src/DetailScreen.js
@@ -1,5 +1,5 @@
 import React, { Component, useState, useEffect } from 'react';
-import { View, Text, Button, SafeAreaView, FlatList, StyleSheet, StatusBar, Dimensions } from 'react-native';
+import { View, Image, Text, Button, SafeAreaView, FlatList, StyleSheet, StatusBar, Dimensions, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 
 const screenWidth = Dimensions.get("window").width;
@@ -9,18 +9,36 @@ import { LineChart } from "react-native-chart-kit";
 const converter = require("xml-js");
 var parseString = require("react-native-xml2js").parseString;
 
+// svg
+// import SVGImg from './assets/ic_arrow_right.svg';
+// import { SvgXml } from 'react-native-svg';
+
 
 const DetailScreen = ({ navigation }) => {
 
+
+
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [date, setDate] = useState('');
 
     var apiKey = '8AGvK4G3WZpiMp1BW1S6oFQ%2F7xKf3zxlB3d6r6LRXslgH8Q7eJUSnP8%2FKd1BEpaQyieYG2aUbXiNzCW%2Ba0%2Bwaw%3D%3D'
-    var url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=' + apiKey + '&pageNo=1&numOfRows=10&startCreateDt=20220417&endCreateDt=20220417'
+    var url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey='
+        + apiKey
+        + '&pageNo=1'
+        + '&numOfRows=10'
+        + '&startCreateDt=' + getToday()
+        + '&endCreateDt=' + getToday();
+    var urlSub = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey='
+        + apiKey
+        + '&pageNo=1'
+        + '&numOfRows=10';
 
-    const getData = () => {
+    const getData = (param) => {
+        console.log(param)
         setLoading(true);
-        axios.get(url)
+        axios.get(urlSub + '&' + 'startCreateDt=' + param + '&endCreateDt=' + param)
+            // axios.get(url)
             .then((response) => {
                 const result = response.data.response.body.items.item
                 console.log(result)
@@ -32,25 +50,56 @@ const DetailScreen = ({ navigation }) => {
     }
 
     useEffect(() => {
-        getData();
+        setDate(getToday())
+        getData(getToday());
     }, []);
+
+
+    const leftClick = () => {
+        var dateNumber = Number(date);
+        dateNumber--;
+        setDate(String(dateNumber))
+        getData(String(dateNumber));
+    }
+
+    const rightClick = () => {
+        var dateNumber = Number(date);
+        dateNumber++;
+        setDate(String(dateNumber))
+        getData(String(dateNumber));
+    }
+
 
 
     return (
         <SafeAreaView style={styles.container}>
-            { <FlatList
-                data={data}
-                renderItem={renderItem}
-                keyExtractor={(item) => String(item.seq)}
-            />}
-            {/* <Text>{asd}</Text> */}
-            {/* <LineChart
-                data={sample}
-                width={screenWidth}
-                height={220}
-                chartConfig={chartConfig}
-            /> */}
-        </SafeAreaView>
+            <View style={{ flexDirection: 'column' }}>
+                <View style={styles.container, { padding: 20, flexDirection: 'row', width: '100%' }} >
+                    <View style={{ flex: 1, alignItems: 'center' }}>
+                        <TouchableOpacity style={styles.iconbutton} onPress={() => leftClick()}>
+                            <Image
+                                source={require('./icon/ic_arrow_left.png')}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ flex: 2, alignItems: 'center' }}>
+                        <Text style={{ color: 'black' }}>{date}</Text>
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <TouchableOpacity style={styles.iconbutton} onPress={() => rightClick()}>
+                            <Image
+                                source={require('./icon/ic_arrow_right.png')}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <FlatList style={{ marginBottom: 150 }}
+                    data={data}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => String(item.seq)}
+                />
+            </View>
+        </SafeAreaView >
     );
 }
 export default DetailScreen;
@@ -84,6 +133,9 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 32,
     },
+    iconbutton: {
+        margin: 10,
+    },
 });
 
 /**
@@ -110,3 +162,13 @@ const sample = {
     ],
     legend: ["COVID-19"] // optional
 };
+
+
+const getToday = () => {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (1 + date.getMonth())).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+
+    return year + month + day;
+}
