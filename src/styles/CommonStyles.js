92e6c180f11852a9026cd6473689fd0aa0a91b12
이지원 <uk9336@naver.com>
2022-05-02 23:10:24 +0900

    * 공통 StyleSheet 생성

---
+++ src/styles/CommonStyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+/**
+ * 공통으로 사용되는 StyleSheet
+ */
+module.exports = StyleSheet.create({
+    space: {
+        height: 15,
+        backgroundColor: "#2c2929"
+    },
+    space_black: {
+        height: 15,
+        backgroundColor: "#1C1B1B"
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#1C1B1B"
+    },
+});
